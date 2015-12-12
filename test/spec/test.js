6397d0212b12d472de777712818090a8eb4491a5
Mohannad Abu-Shamsieh <mohannad@foundertherapy.co>
2015-12-13 00:24:45 +0200

    implemented the feature x

--- test/spec/test.js
+++ test/spec/test.js
@@ -4,7 +4,7 @@
   describe('Give it some context', function () {
     describe('maybe a bit more context here', function () {
       it('should run here few assertions', function () {
-        chai.assert.equal(true, true);
+        chai.assert.equal(false, true);
       });
     });
   });
